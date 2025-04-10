c7e5fa02785ed9a960cef71b247fe12230dd0d72
Devnatavaz <contato.natavaz@gmail.com>
2025-04-10 14:07:30 -0300

    Validação de form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,42 +1,33 @@
 // pages/Login.tsx
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import React, { useState } from "react";
-import Facebook from "/facebook.svg"
-import Google from "/google.svg"
-import Github from "/github.svg"
+import Facebook from "/facebook.svg";
+import Google from "/google.svg";
+import Github from "/github.svg";
 import styles from './Login.module.css';
 
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 function Login() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [isLogin, setIsLogin] = useState(false);
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>();
 
-
-
-  const login = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (name == 'nata' && email == 'natavaz02@gmail.com' && password == '123') {
-      setError('');
+  const onSubmit = ({ name, email, password }: FormData) => {
+    if (email === 'natavaz02@gmail.com' && password === '123') {
       localStorage.setItem("auth", "true");
-      setIsLogin(true);
-
       navigate("/");
     } else {
-      setError('Nome, Email e senhas incorretos');
-      setIsLogin(false);
+      alert("Nome, email ou senha incorretos.");
     }
-
-  }
-
-  // if (isLogin) {
-  //   return (
-  //     navigate("/")
-  //   )
-  // }
+  };
 
   return (
     <main>
@@ -45,31 +36,28 @@ function Login() {
           <h1>Login</h1>
           <i className="fa-solid fa-moon"></i>
         </div>
+
         <div className={styles.containerLinks}>
           <a href="/"><img src={Facebook} alt="facebook logo" /></a>
           <a href="/"><img src={Google} alt="google logo" /></a>
           <a href="/"><img src={Github} alt="github logo" /></a>
         </div>
 
-        {error && <p style={{ color: 'red', marginBottom: '10px' }}>{error}</p>}
-
-        <div className={styles.inputs}>
-          <div className={styles.inputBox}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.inputs}>
+          {/* <div className={styles.inputBox}>
             <label htmlFor="name">
               Name
               <div className={styles.inputField}>
                 <i className="fa-solid fa-user"></i>
                 <input
-                  type="text"
                   id="name"
-                  name="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  {...register("name", { required: "Nome é obrigatório" })}
                 />
               </div>
+              {errors.name && <span style={{ color: "red" }}>{errors.name.message}</span>}
             </label>
-          </div>
-
+          </div> */}
 
           <div className={styles.inputBox}>
             <label htmlFor="email">
@@ -77,16 +65,20 @@ function Login() {
               <div className={styles.inputField}>
                 <i className="fa-solid fa-envelope"></i>
                 <input
-                  type="email"
                   id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  {...register("email", {
+                    required: "E-mail é obrigatório",
+                    pattern: {   
+                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                      message: "E-mail inválido"
+                    }
+                  })}
                 />
               </div>
+              {errors.email && <span style={{ color: "red" }}>{errors.email.message}</span>}
             </label>
           </div>
-
 
           <div className={styles.inputBox}>
             <label htmlFor="password">
@@ -94,36 +86,29 @@ function Login() {
               <div className={styles.inputField}>
                 <i className="fa-solid fa-key"></i>
                 <input
-                  type="password"
                   id="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  {...register("password", {
+                    required: "Senha é obrigatória",
+                    minLength: { value: 3, message: "Mínimo de 3 caracteres" }
+                  })}
                 />
               </div>
+              {errors.password && <span style={{ color: "red" }}>{errors.password.message}</span>}
+              <div className={styles.forgot_password}>
+                <a href="#">Forgot your password?</a>
+              </div>
             </label>
-
-
-            <div className={styles.forgot_password}>
-              <a href="#">
-                Forgot your password?
-              </a>
-            </div>
           </div>
-        </div>
 
-
-        <button
-          type="submit"
-          className={styles.login_button}
-          onClick={login}>
-
-          Login
-        </button>
+          <button className={styles.loginButton}>
+            Login
+          </button>
+          <a className={styles.cadastroLogin} href="/cadastro">Criar Conta</a>
+        </form>
       </div>
     </main>
   );
 }
-
 
 export default Login;
